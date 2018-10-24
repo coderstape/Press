8bac35d82c55546dca106cf9f74b60e81b0e1f45
Victor Gonzalez <vicgonvt@gmail.com>
2018-10-23 21:44:58 -0400

    bring in the parser

---
+++ src/MarkdownParser.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace vicgonvt\LaraPress;
+
+use Parsedown;
+
+class MarkdownParser
+{
+    /**
+     * Given a markdown string, it will pass back a parsed string.
+     * 
+     * @param $text
+     *
+     * @return string
+     */
+    public static function parse($text)
+    {
+        return Parsedown::instance()->text($text);
+    }
+}

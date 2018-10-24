3f40e54aa480cabb008c99023207ef4d2b2863b8
Victor Gonzalez <vicgonvt@gmail.com>
2018-10-23 20:26:18 -0400

    wip package structure

---
+++ tests/Feature/InitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace vicgonvt\LaraPress\Tests;
+
+class InitTest extends TestCase
+{
+    /** @test */
+    public function test_inits()
+    {
+        $this->assertTrue(true);
+    }
+}

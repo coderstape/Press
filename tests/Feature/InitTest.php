7ba3f578c8c5b4c1708a06ee309f9a04e1cdb5c1
Victor Gonzalez <vicgonvt@gmail.com>
2018-10-24 23:36:00 -0400

    test cleanup

--- tests/Feature/InitTest.php
+++ tests/Feature/InitTest.php
@@ -2,25 +2,13 @@
 
 namespace vicgonvt\LaraPress\Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\File;
-use vicgonvt\LaraPress\PressFileParser;
 
 class InitTest extends TestCase
 {
-    use RefreshDatabase;
-
     /** @test */
     public function it_can_find_the_test_markdown_files()
     {
         $this->assertTrue(File::exists(__DIR__ . '/../stubs/MarkFile1.md'));
     }
-
-    /** @test */
-//    public function experiment()
-//    {
-//        $data = (new PressFileParser(__DIR__ . '/../stubs/MarkFile1.md'));
-//
-//        die(var_dump($data->head()));
-//    }
 }

a9acfe3f31382e63de10ec803d409fc111f16413
Victor Gonzalez <vicgonvt@gmail.com>
2018-10-24 23:36:39 -0400

    Update src/LaraPressServiceProvider.php

--- src/LaraPressServiceProvider.php
+++ src/LaraPressServiceProvider.php
@@ -8,7 +8,7 @@ use vicgonvt\LaraPress\Console\ProcessCommand;
 class LaraPressServiceProvider extends ServiceProvider
 {
     protected $packageName = 'LaraPress';
-    
+
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');

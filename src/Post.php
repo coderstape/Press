91e7e968a0d5afd66817be59172235308c728d03
Victor Gonzalez <vicgonvt@gmail.com>
2018-10-27 15:31:50 -0400

    update the post model

--- src/Post.php
+++ src/Post.php
@@ -4,20 +4,55 @@ namespace vicgonvt\LaraPress;
 
 class Post extends Model
 {
+    /**
+     * @var array
+     */
     protected $guarded = [];
 
+    /**
+     * The attributes that should be typecasted as an instance of Carbon.
+     *
+     * @var array
+     */
     protected $dates = ['published_at'];
 
+    /**
+     * Get the fully qualified path to this post.
+     *
+     * @return \Illuminate\Contracts\Routing\UrlGenerator|string
+     */
+    public function path()
+    {
+        return url(LaraPress::path() . "/posts/{$this->id}");
+    }
+
+    /**
+     * Scope the posts to only those set to active.
+     *
+     * @param $query
+     *
+     * @return mixed
+     */
     public function scopeActive($query)
     {
         return $query->where('active', 1);
     }
 
+    /**
+     * Get the series that this post belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function series()
     {
         return $this->belongsTo(Series::class);
     }
 
+    /**
+     * Get the tags that this post is tagged with.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function tags()
     {
         return $this->belongsToMany(Tag::class, $this->prefix . 'post_tag');

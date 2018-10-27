a0787b490fced686146f8bfd163fb6404689ab82
Victor Gonzalez <vicgonvt@gmail.com>
2018-10-26 23:56:35 -0400

    Update src/LaraPressServiceProvider.php

--- src/LaraPressServiceProvider.php
+++ src/LaraPressServiceProvider.php
@@ -2,21 +2,87 @@
 
 namespace vicgonvt\LaraPress;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
-use vicgonvt\LaraPress\Console\ProcessCommand;
 
 class LaraPressServiceProvider extends ServiceProvider
 {
-    protected $packageName = 'LaraPress';
-
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        if ($this->app->runningInConsole()) {
+            $this->registerPublishing();
+        }
 
+        $this->registerResources();
+    }
+
+    /**
+     * Register the package resources such as routes, templates, etc.
+     *
+     * @return void
+     */
+    protected function registerResources()
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'larapress');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->registerRoutes();
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__.'/LaraPressServiceProvider.php' => app_path('Providers/LaraPressServiceProvider.php'),
+        ], 'larapress-provider');
+        $this->publishes([
+            __DIR__.'/../config/larapress.php' => config_path('larapress.php'),
+        ], 'larapress-config');
+    }
+
+    /**
+     * Register the package routes.
+     *
+     * @return void
+     */
+    protected function registerRoutes()
+    {
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        });
+    }
+
+    /**
+     * Get the LaraPress route group configuration array.
+     *
+     * @return array
+     */
+    protected function routeConfiguration()
+    {
+        return [
+            'namespace' => 'vicgonvt\LaraPress\Http\Controllers',
+            'prefix' => 'blog',
+        ];
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $this->commands([
-            ProcessCommand::class,
+            Console\ProcessCommand::class,
         ]);
-
-        $this->mergeConfigFrom( __DIR__.'/../config/larapress.php', strtolower($this->packageName));
     }
 }

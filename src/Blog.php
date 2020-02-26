4377edd6fe244589cc2a2fd9e73e022cd2bfed34
Victor Gonzalex <vicgonvt@gmail.com>
2020-02-26 08:38:05 -0500

    adds path helper to raw blogs table

--- src/Blog.php
+++ src/Blog.php
@@ -2,10 +2,22 @@
 
 namespace coderstape\Press;
 
+use coderstape\Press\Facades\Press;
+
 class Blog extends Model
 {
     /**
      * @var array
      */
     protected $guarded = [];
+
+    /**
+     * Returns a fully qualified path to this blog resource.
+     *
+     * @return \Illuminate\Contracts\Routing\UrlGenerator|string
+     */
+    public function path()
+    {
+        return url(Press::path() . "/blog/{$this->id}");
+    }
 }

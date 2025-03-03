50050d5fb1920f2e163ad42f75f66a454d43989b
Victor Gonzalez <vicgonvt@gmail.com>
2025-03-03 14:26:19 -0500

    Update DatabaseDriver.php

--- src/Drivers/DatabaseDriver.php
+++ src/Drivers/DatabaseDriver.php
@@ -18,9 +18,9 @@ class DatabaseDriver extends Driver
         $blogs = Blog::all();
 
         $blogs->each(function ($blog) {
-            if ($blog->updated_at > now()->subDay(7)) {
+//            if ($blog->updated_at > now()->subDay(7)) {
                 $this->parse($blog->data, $blog->id);
-            }
+//            }
         });
 
         return $this->posts;

8b75d9a066de02c5c969974fc7865ddb69779cc2
Victor Gonzalez <vicgonvt@gmail.com>
2025-07-11 18:04:26 -0400

    change the order of author posts

--- src/Http/Controllers/AuthorController.php
+++ src/Http/Controllers/AuthorController.php
@@ -16,7 +16,7 @@ class AuthorController extends Controller
      */
     public function index()
     {
-        $authors = Author::whereHas('activePosts')->orderBy('published_at', 'desc')->get();
+        $authors = Author::whereHas('activePosts')->orderBy('published_at', 'asc')->get();
 
         return theme('authors.index', compact('authors'));
     }
